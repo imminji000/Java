d584e782e46e745040cdfd486e2b2e5a1cab1e83
swlss7979 <swlss7979@gmail.com>
2022-08-31 10:40:20 +0900

    2022/08/31 오버라이드예제추기

---
+++ Ch05/src/sub6/Sedan.java
@@ -0,0 +1,32 @@
+package sub6;
+
+public class Sedan extends Car {
+
+	
+	private int cc;
+	
+	public Sedan(String name, String color, int speed)  {
+		super(name, color, speed);
+		this.cc = cc;
+	}
+	
+	@Override
+	public void speedUp(int speed) {
+		this.speed += speed + 20; 
+	}
+	
+	
+	public void speedTurbo()    {
+		this.speed  += 30;
+				
+	}
+	
+	public void show()  {
+		System.out.println("차랑명 : "+name);
+		System.out.println("차량색 : "+color);
+		System.out.println("차량속도 : "+speed);
+		System.out.println("배기량 : "+cc);
+		
+		
+	}
+}
