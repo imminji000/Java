45b7b12f7c7c15f6669274a087d9efefd62ab742
swlss7979 <swlss7979@gmail.com>
2022-09-19 10:40:58 +0900

    2022/09/15 트랜잭션 실습 추가

---
+++ Ch11/src/sub2/UpdateTest.java
@@ -0,0 +1,52 @@
+package sub2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class UpdateTest {
+         public static void main(String[] args) {
+			
+        	 
+        	//DB정보
+             String host = "jdbc:mysql://127.0.0.1:3306/java1db";
+        	   String user = "root";
+        	   String pass = "1234";
+  			
+        	try { 
+        //1단계 -JDBC 드라이버 로드
+        		Class.forName("com.mysql.cj.jdbc.Driver");
+        		
+		 //2단계 -데이터베이스 접속
+        		Connection conn = DriverManager.getConnection(host,user,pass);
+        		
+         //3단계 -SQL 실행객체 생성
+        		 Statement stmt = conn.createStatement();
+        		
+         //4단계 -SQL 실행
+         String sql = "UPDATE `User1` SET `name` = '홍길동', 'hp' = '010-1111=1111' " ;
+        		sql += "WHERE `uid = 'a101';";
+        		
+        		
+        		stmt.executeUpdate(sql);
+        		
+         //5단계
+         //6단계 -데이터베이스 종료
+        		stmt.close();
+       		    conn.close();
+       		 
+        		
+        		
+        	}catch (Exception e)  {
+        		e.printStackTrace();
+        	}
+        		
+        		System.out.println("Update 완료...");
+        		
+         }     
+}
+
+
+
+
+
