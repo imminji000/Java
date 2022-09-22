3d2f80b156913a3c62320b9dd1dd44bc7ec5ffbb
swlss7979 <swlss7979@gmail.com>
2022-09-22 13:28:25 +0900

    2022/09/22 수업내용

---
+++ Ch15/src/sub2/ComponentTest.java
@@ -0,0 +1,264 @@
+package sub2;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import java.awt.Button;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
+
+public class ComponentTest extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtf1;
+	private JTextField txtf2;
+	private JTextField txtf3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ComponentTest frame = new ComponentTest();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ComponentTest() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 492);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("컴포넌트 실습하기");
+		lblNewLabel.setFont(new Font("굴림", Font.BOLD, 14));
+		lblNewLabel.setBounds(12, 10, 163, 26);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Button 컴포넌트");
+		lblNewLabel_1.setBounds(22, 41, 100, 15);
+		contentPane.add(lblNewLabel_1);
+		
+		JButton btn1 = new JButton("버튼1");
+		btn1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("버튼1 클릭...");
+			}
+		});
+		btn1.setBounds(32, 66, 68, 23);
+		contentPane.add(btn1);
+		
+		JButton btn2 = new JButton("버튼2");
+		btn2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "버튼2 클릭...");
+			}
+		});
+		btn2.setBounds(100, 66, 68, 23);
+		contentPane.add(btn2);
+		
+		JButton btn3 = new JButton("버튼3");
+		btn3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "버튼3 클릭...", "대화상자 제목", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+		btn3.setBounds(168, 66, 68, 23);
+		contentPane.add(btn3);
+		
+		JButton btn4 = new JButton("버튼4");
+		btn4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int answer = JOptionPane.showConfirmDialog(null, "버튼4 클릭...", "확인대화상자", JOptionPane.YES_NO_OPTION);
+				
+				if(answer == JOptionPane.YES_OPTION) {
+					System.out.println("Yes 클릭...");
+				}else {
+					System.out.println("No 클릭...");
+				}
+			}
+		});
+		btn4.setBounds(236, 66, 68, 23);
+		contentPane.add(btn4);
+		
+		JLabel lblNewLabel_2 = new JLabel("TextField 컴포넌트");
+		lblNewLabel_2.setBounds(22, 106, 123, 15);
+		contentPane.add(lblNewLabel_2);
+		
+		txtf1 = new JTextField();
+		txtf1.setBounds(32, 131, 116, 21);
+		contentPane.add(txtf1);
+		txtf1.setColumns(10);
+		
+		txtf2 = new JTextField();
+		txtf2.setColumns(10);
+		txtf2.setBounds(32, 156, 116, 21);
+		contentPane.add(txtf2);
+		
+		txtf3 = new JTextField();
+		txtf3.setColumns(10);
+		txtf3.setBounds(32, 181, 116, 21);
+		contentPane.add(txtf3);
+		
+		JLabel lblTxtf1Result = new JLabel("결과 :");
+		lblTxtf1Result.setBounds(214, 134, 132, 15);
+		contentPane.add(lblTxtf1Result);
+		
+		JLabel lblTxtf2Result = new JLabel("결과 :");
+		lblTxtf2Result.setBounds(214, 159, 132, 15);
+		contentPane.add(lblTxtf2Result);
+		
+		JLabel lblTxtf3Result = new JLabel("결과 :");
+		lblTxtf3Result.setBounds(214, 184, 132, 15);
+		contentPane.add(lblTxtf3Result);
+		
+		
+		JButton btnTxtf1 = new JButton("확인");
+		btnTxtf1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String txt = txtf1.getText();
+				lblTxtf1Result.setText("결과 : " + txt);				
+			}
+		});
+		btnTxtf1.setBounds(149, 130, 60, 23);
+		contentPane.add(btnTxtf1);
+		
+		JButton btnTxtf2 = new JButton("확인");
+		btnTxtf2.setBounds(149, 155, 60, 23);
+		contentPane.add(btnTxtf2);
+		
+		JButton btnTxtf3 = new JButton("확인");
+		btnTxtf3.setBounds(149, 180, 60, 23);
+		contentPane.add(btnTxtf3);
+		
+		JLabel lblNewLabel_3 = new JLabel("CheckBox 컴포넌트");
+		lblNewLabel_3.setBounds(22, 222, 123, 15);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("취미를 고르세요.");
+		lblNewLabel_4.setBounds(32, 241, 136, 15);
+		contentPane.add(lblNewLabel_4);
+		
+		JCheckBox chk1 = new JCheckBox("등산");
+		chk1.setBounds(30, 262, 60, 23);
+		contentPane.add(chk1);
+		
+		JCheckBox chk2 = new JCheckBox("여행");
+		chk2.setBounds(94, 262, 60, 23);
+		contentPane.add(chk2);
+		
+		JCheckBox chk3 = new JCheckBox("독서");
+		chk3.setBounds(152, 262, 60, 23);
+		contentPane.add(chk3);
+		
+		JCheckBox chk4 = new JCheckBox("운동");
+		chk4.setBounds(216, 262, 60, 23);
+		contentPane.add(chk4);
+		
+		JCheckBox chk5 = new JCheckBox("게임");
+		chk5.setBounds(280, 262, 60, 23);
+		contentPane.add(chk5);
+		
+		JLabel lblChkResult = new JLabel("결과 : ");
+		lblChkResult.setBounds(32, 291, 314, 15);
+		contentPane.add(lblChkResult);
+		
+		JButton btnChk = new JButton("확인");
+		btnChk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				List<String> chks = new ArrayList<>();
+				
+				if(chk1.isSelected()) {
+					chks.add(chk1.getText());
+				}
+				
+				if(chk2.isSelected()) {
+					chks.add(chk2.getText());
+				}
+				
+				if(chk3.isSelected()) {
+					chks.add(chk3.getText());
+				}
+				
+				if(chk4.isSelected()) {
+					chks.add(chk4.getText());
+				}
+				
+				if(chk5.isSelected()) {
+					chks.add(chk5.getText());
+				}
+				
+				lblChkResult.setText("결과 : " + chks.toString());
+			}
+		});
+		btnChk.setBounds(348, 262, 60, 23);
+		contentPane.add(btnChk);
+		
+		JLabel lblNewLabel_5 = new JLabel("RadioButton 컴포넌트");
+		lblNewLabel_5.setBounds(22, 326, 153, 15);
+		contentPane.add(lblNewLabel_5);
+		
+		JRadioButton rdMale = new JRadioButton("남자");
+		rdMale.setBounds(27, 347, 68, 23);
+		contentPane.add(rdMale);
+		
+		JRadioButton rdFemale = new JRadioButton("여자");
+		rdFemale.setBounds(100, 347, 68, 23);
+		contentPane.add(rdFemale);
+		
+		ButtonGroup bg = new ButtonGroup();
+		bg.add(rdFemale);
+		bg.add(rdMale);
+		
+		JLabel lblGenderResult = new JLabel("결과 :");
+		lblGenderResult.setBounds(32, 376, 123, 15);
+		contentPane.add(lblGenderResult);
+		
+		JButton btnGender = new JButton("확인");
+		btnGender.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {				
+												
+				if(rdMale.isSelected()) {
+					lblGenderResult.setText("결과 : 남자");
+				}else {
+					lblGenderResult.setText("결과 : 여자");
+				}
+				
+			}
+		});
+		btnGender.setBounds(176, 347, 60, 23);
+		contentPane.add(btnGender);
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ Ch15/src/sub3/CalculatorTest.java
@@ -0,0 +1,124 @@
+package sub3;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class CalculatorTest extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CalculatorTest frame = new CalculatorTest();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CalculatorTest() {
+		setTitle("나의 계산기 v1.0");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 244, 298);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setFont(new Font("굴림", Font.BOLD, 16));
+		textField.setHorizontalAlignment(SwingConstants.RIGHT);
+		textField.setText("0");
+		textField.setBounds(12, 10, 192, 37);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("7");
+		btnNewButton.setBounds(12, 57, 40, 40);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("8");
+		btnNewButton_1.setBounds(62, 57, 40, 40);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("9");
+		btnNewButton_2.setBounds(114, 57, 40, 40);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("/");
+		btnNewButton_3.setBounds(166, 57, 40, 40);
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("4");
+		btnNewButton_4.setBounds(12, 107, 40, 40);
+		contentPane.add(btnNewButton_4);
+		
+		JButton btnNewButton_1_1 = new JButton("5");
+		btnNewButton_1_1.setBounds(62, 107, 40, 40);
+		contentPane.add(btnNewButton_1_1);
+		
+		JButton btnNewButton_2_1 = new JButton("6");
+		btnNewButton_2_1.setBounds(114, 107, 40, 40);
+		contentPane.add(btnNewButton_2_1);
+		
+		JButton btnNewButton_3_1 = new JButton("X");
+		btnNewButton_3_1.setBounds(166, 107, 40, 40);
+		contentPane.add(btnNewButton_3_1);
+		
+		JButton btnNewButton_5 = new JButton("1");
+		btnNewButton_5.setBounds(12, 157, 40, 40);
+		contentPane.add(btnNewButton_5);
+		
+		JButton btnNewButton_1_2 = new JButton("2");
+		btnNewButton_1_2.setBounds(62, 157, 40, 40);
+		contentPane.add(btnNewButton_1_2);
+		
+		JButton btnNewButton_2_2 = new JButton("3");
+		btnNewButton_2_2.setBounds(114, 157, 40, 40);
+		contentPane.add(btnNewButton_2_2);
+		
+		JButton btnNewButton_3_2 = new JButton("-");
+		btnNewButton_3_2.setBounds(166, 157, 40, 40);
+		contentPane.add(btnNewButton_3_2);
+		
+		JButton btnNewButton_6 = new JButton("0");
+		btnNewButton_6.setBounds(12, 207, 40, 40);
+		contentPane.add(btnNewButton_6);
+		
+		JButton btnNewButton_1_3 = new JButton("C");
+		btnNewButton_1_3.setFont(new Font("굴림", Font.PLAIN, 10));
+		btnNewButton_1_3.setBounds(62, 207, 40, 40);
+		contentPane.add(btnNewButton_1_3);
+		
+		JButton btnNewButton_2_3 = new JButton("=");
+		btnNewButton_2_3.setBounds(114, 207, 40, 40);
+		contentPane.add(btnNewButton_2_3);
+		
+		JButton btnNewButton_3_3 = new JButton("+");
+		btnNewButton_3_3.setBounds(166, 207, 40, 40);
+		contentPane.add(btnNewButton_3_3);
+	}
+
+}
+Footer
+© 2022 GitHub, Inc.
