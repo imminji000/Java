13e7b360993a51e1385c2b40547e281c3dcc99ef
imminji000 <swlss79@naver.com>
2023-02-27 13:45:13 +0900

    2023/02/27 Ch01 수정

--- Book1/src/Ch01/p46.java
+++ Book1/src/Ch01/p46.java
@@ -5,6 +5,8 @@ public class p46 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Java");
+		System.out.println("Welcome Java");
+		
 		
 		System.out.println("콘솔 화면에 출력");
 		System.out.println("Hello World");
--- Book1/src/Ch05/Person.java
+++
@@ -1,9 +0,0 @@
-package Ch05;
-
-public class Person {
-	String name;
-	int height;
-	double weight;
-	char gender;
-	boolean married;
-}
